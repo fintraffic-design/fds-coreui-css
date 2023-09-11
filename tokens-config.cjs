db15bd878d52c687e37089f191fa01c55247498d
Roope Lindström <roope.lindstrom@solita.fi>
2023-09-11 14:53:03 +0300

    AT-79: Add style generation for Lit

--- tokens-config.cjs
+++ tokens-config.cjs
@@ -16,6 +16,20 @@ function fdsTokenString(token) {
   return `{ name: "fds-${paramCase(token.name)}", value: "${token.value}" }`;
 }
 
+function capitalize(word) {
+  return `${word.charAt(0).toUpperCase()}${word.slice(1)}`
+}
+
+function uncapitalize(word) {
+  return `${word.charAt(0).toLowerCase()}${word.slice(1)}`
+}
+
+function indent(size = 2) {
+  return function(line) {
+    return `${' '.repeat(size)}${line}`
+  }
+}
+
 module.exports = {
   source: ['src/tokens/**/*.json'],
   format: {
@@ -59,6 +73,59 @@ module.exports = {
         .join(";\n")
 
       return `:root {\n${variables}\n}`
+    },
+    'fds/lit': ({ dictionary }) => {
+      const { Typography } = dictionary.properties
+
+      const styles = Object.entries(Typography).flatMap(([groupName, group]) => (
+        Object.entries(group).map(([styleName, style]) => ({
+          className: Array.from(
+            new Set(
+              [groupName, styleName, 'text']
+                .join(' ')
+                .split(' ')
+            )
+          )
+            .join('-')
+            .toLowerCase(),
+          properties: Object.entries(style).map(([property, { name }]) => ({ property, token: `Fds${name}` })),
+        }))
+      ))
+
+      return [
+        'import {',
+        styles
+          .flatMap(style => style.properties.map(property => property.token))
+          .sort()
+          .map(indent())
+          .join(',\n'),
+        `} from './tokens'\n`,
+        `import { tokenVar } from './token-utils'`,
+        `import { css } from 'lit'\n`,
+        ...styles
+          .sort((a, b) => a.className.localeCompare(b.className))
+          .map(style => (
+            [
+              `export const ${
+                uncapitalize(
+                  style.className
+                    .split('-')
+                    .map(capitalize)
+                    .join('')
+                )
+              }Class = css\``,
+              `  .${style.className} {`,
+              style.properties
+                .sort((a, b) => a.property.localeCompare(b.property))
+                .map(({ property, token }) => `${property}: \${tokenVar(${token})};`)
+                .map(indent(4))
+                .join('\n'),
+              '  }\n`\n'
+            ]
+              .join('\n')
+        )),
+      ]
+        .join('\n')
     }
   },
 
@@ -95,6 +162,10 @@ module.exports = {
           destination: 'tokens.js',
           format: 'fds/javascript'
         },
+        {
+          destination: 'lit-styles.js',
+          format: 'fds/lit'
+        }
       ]
     }
   }
